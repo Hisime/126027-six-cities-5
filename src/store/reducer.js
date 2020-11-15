6230f935a32d67abd855644ae0151c543d25a501
unknown <rihisime@gmail.com>
2020-11-15 17:20:05 +0300

    Add offers sort

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,10 +3,12 @@ import cities from '../mocks/cities';
 import {ActionType} from '../store/action';
 import {extend} from '../utils';
 import sortList from '../mocks/sort-list';
+import {sortTypes} from '../consts';
 
 const initialState = {
   currentCity: cities[0],
   offers: offers[0],
+  defaultOffersSort: offers[0],
   currentSort: sortList[0],
   isSortOpen: false,
 };
@@ -15,8 +17,11 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.GET_OFFERS:
       const offerId = action.payload;
+      const defaultOffersSort = offers[offerId];
+      const sortedOffersOnInit = sortOffers(offers[offerId], defaultOffersSort, state.currentSort.value);
       return extend(state, {
-        offers: offers[offerId],
+        offers: sortedOffersOnInit,
+        defaultOffersSort,
       });
     case ActionType.SET_CITY:
       const cityId = action.payload;
@@ -29,11 +34,30 @@ const reducer = (state = initialState, action) => {
       });
     case ActionType.SET_SORT:
       const sortId = action.payload;
+      const sort = sortList[sortId];
+      const sortedOffers = sortOffers(state.offers, state.defaultOffersSort, sort.value);
+
       return extend(state, {
-        currentSort: sortList[sortId]
+        offers: sortedOffers,
+        currentSort: sort,
       });
   }
   return state;
+};
+
+const sortOffers = (offersList, defaultOffersList, sortType) => {
+  const sortedOffers = JSON.parse(JSON.stringify(offersList));
+  switch (sortType) {
+    case sortTypes.POPULAR:
+      return defaultOffersList;
+    case sortTypes.PRICE_ASC:
+      return sortedOffers.sort((a, b) => a.price - b.price);
+    case sortTypes.PRICE_DES:
+      return sortedOffers.sort((a, b) => b.price - a.price);
+    case sortTypes.TOP_DES:
+      return sortedOffers.sort((a, b) => b.rate - a.rate);
+  }
+  return sortedOffers;
 };
 
 
