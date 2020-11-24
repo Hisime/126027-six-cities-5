4bca075c373c97e51472606028c406d41bf676c8
unknown <rihisime@gmail.com>
2020-11-25 00:48:51 +0300

    quick fix

--- src/components/review-form/review-form.test.jsx
+++ src/components/review-form/review-form.test.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import {ReviewForm} from "./review-form";
-import {offersMock} from "../../test-mocks/offers";
 import {noop} from "../../test-mocks/mocks";
 import Rating from "../../mocks/rating";
 
 it(`Should ReviewForm render correctly`, () => {
   const tree = renderer
     .create(<ReviewForm
-      id={offersMock[0].id}
+      id={`1`}
       sendComment={noop}
       ratingList={Rating}/>)
     .toJSON();
